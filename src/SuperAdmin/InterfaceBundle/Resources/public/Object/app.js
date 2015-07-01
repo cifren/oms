cc8a2362d88dae96db83c615299c1024d609bcd1
cifren <cifren@msn.com>
2015-06-30 21:29:09 -0300

    Edit javascipt on object CRUD page

--- src/SuperAdmin/InterfaceBundle/Resources/public/Object/app.js
+++ src/SuperAdmin/InterfaceBundle/Resources/public/Object/app.js
@@ -1,49 +1,37 @@
 var objectApp = angular.module('objectApp', [
-  'ngResource',
-  'ui.bootstrap',
-  'clientsControllers'
+    'ngResource',
+    'ngRoute',
+    'controllers'
 ]);
 
 objectApp
         .config(['$routeProvider',
-          function($routeProvider) {
-            $routeProvider.
-                    //advanced search list
-                    when('/advancedSearch/clients/name=:name&birthDate=:birthDate&fininstitut=:fininstitut&panNumber=:panNumber&accountNumber=:accountNumber&branch=:branch', {
-                      name: 'advancedClientList',
-                      templateUrl: Routing.generate('cr_frontend_client_template', {templateName: 'clientList.html.twig'}),
-                      controller: 'ClientListAdvCtrl'
-                    }).
-                    //details client from advanced list
-                    when('/advancedSearch/clientDetail/:clientId/name=:name&birthDate=:birthDate&fininstitut=:fininstitut&panNumber=:panNumber&accountNumber=:accountNumber&branch=:branch', {
-                      templateUrl: Routing.generate('cr_frontend_client_template', {templateName: 'clientDetails.html.twig'}),
-                      controller: 'ClientDetailsAdvCtrl'
-                    }).
-                    otherwise({
-                      redirectTo: '/'
-                    });
-          }
-        ])
-        .run(function($route, $rootScope) {
-          $rootScope.path = function(controller, params)
-          {
-            // Iterate over all available routes
-            for (var path in $route.routes)
-            {
-              var nameController = $route.routes[path].name;
-              if (nameController === controller) // Route found
-              {
-                var result = path;
-                // Construct the path with given parameters in it
-                for (var param in params)
-                {
-                  result = result.replace(':' + param, params[param]);
-                }
-                return result;
-              }
+            function ($routeProvider) {
+                $routeProvider.
+                        when('/list', {
+                            name: 'list',
+                            templateUrl: Routing.generate('api_templateaccess',
+                                    {templateName: 'SuperAdminInterfaceBundle:Object:list.html.twig'}
+                            ),
+                            controller: 'List'
+                        }).
+                        when('/create', {
+                            name: 'create',
+                            templateUrl: Routing.generate('api_templateaccess',
+                                    {templateName: encodeURIComponent('SuperAdminInterfaceBundle:Object:edit.html.twig')}
+                            ),
+                            controller: 'Create'
+                        }).
+                        when('/edit', {
+                            name: 'edit',
+                            templateUrl: Routing.generate('api_templateaccess',
+                                    {templateName: encodeURIComponent('SuperAdminInterfaceBundle:Object:edit.html.twig')}
+                            ),
+                            controller: 'Edit'
+                        }).
+                        otherwise({
+                            redirectTo: '/list'
+                        });
             }
-            // No such controller in route definitions
-            return undefined;
-          };
-        });
+        ]);
 ;
--- src/SuperAdmin/InterfaceBundle/Resources/public/Object/controller.js
+++ src/SuperAdmin/InterfaceBundle/Resources/public/Object/controller.js
@@ -1,92 +1,17 @@
-var clientsControllers = angular.module('clientsControllers', []);
+var controllers = angular.module('controllers', []);
 
-clientsControllers.controller('SearchListCtrl', ['$scope', '$location', '$timeout', '$filter', 'myShareModel',
-  function SearchListCtrl($scope, $location, $timeout, $filter, myShareModel) {
-    $scope.search = myShareModel.getModel();
-
-    $scope.updateAdvancedClients = function() {
-      if (angular.isString($scope.search.birthDate) && $scope.search.birthDate !== '') {
-        $scope.search.birthDate = new Date($scope.search.birthDate);
-      } else if (angular.isDate($scope.search.birthDate)) {
-        $scope.search.birthDate = $filter('date')($scope.search.birthDate, 'yyyy-MM-dd');
-      } else {
-        $scope.search.birthDate = '';
-      }
-      myShareModel.setAdvancedQuery();
-      $location.path('/advancedSearch/clients/' + $scope.search.advancedQuery);
-    };
-    $scope.open = function() {
-      $timeout(function() {
-        $scope.opened = true;
-      });
-    };
-  }]);
-
-clientsControllers.controller('ClientListAdvCtrl', ['$scope', '$http', '$routeParams', 'myShareModel', '$filter',
-  function ClientListAdvCtrl($scope, $http, $routeParams, myShareModel, $filter) {
-    $scope.display = true;
-    $scope.search = myShareModel.getModel();
-
-    if ($routeParams.name !== 'undefined' && $routeParams.name !== 'null') {
-      $scope.search.name = $routeParams.name;
+controllers.controller('List', ['$scope', '$location',
+    function List($scope, $location) {
+        console.log('list');
     }
-    if ($routeParams.birthDate !== 'undefined' && $routeParams.birthDate !== 'null' && $routeParams.birthDate !== '') {
-      $scope.search.birthDate = $filter('date')(new Date($routeParams.birthDate.replace(/-/g, '/')), 'yyyy/MM/dd');
+]);
+controllers.controller('Edit', ['$scope', '$location',
+    function List($scope, $location) {
+        console.log('edit');
     }
-    if ($routeParams.fininstitut !== 'undefined' && $routeParams.fininstitut !== 'null') {
-      $scope.search.fininstitut = $routeParams.fininstitut;
+]);
+controllers.controller('Create', ['$scope', '$location',
+    function List($scope, $location) {
+        console.log('create');
     }
-    if ($routeParams.panNumber !== 'undefined' && $routeParams.panNumber !== 'null') {
-      $scope.search.panNumber = $routeParams.panNumber;
-    }
-    if ($routeParams.accountNumber !== 'undefined' && $routeParams.accountNumber !== 'null') {
-      $scope.search.accountNumber = $routeParams.accountNumber;
-    }
-    if ($routeParams.branch !== 'undefined' && $routeParams.branch !== 'null') {
-      $scope.search.branch = $routeParams.branch;
-    }
-    //share data with searchCtrl
-    myShareModel.setAdvancedQuery();
-
-    if ($routeParams.name === 'undefined') {
-      $routeParams.name = '';
-    }
-    if ($routeParams.birthDate === 'undefined' || $routeParams.birthDate === 'null') {
-      $routeParams.birthDate = '';
-    }
-    if ($routeParams.fininstitut === 'undefined') {
-      $routeParams.fininstitut = '';
-    }
-    if ($routeParams.panNumber === 'undefined') {
-      $routeParams.panNumber = '';
-    }
-    if ($routeParams.accountNumber === 'undefined') {
-      $routeParams.accountNumber = '';
-    }
-    if ($routeParams.branch === 'undefined') {
-      $routeParams.branch = '';
-    }
-
-    $http.get(Routing.generate('cr_frontend_client_list_adv', {'name': $routeParams.name, 'birthDate': $routeParams.birthDate, 'fininstitut': $routeParams.fininstitut, 'panNumber': $routeParams.panNumber, 'accountNumber': $routeParams.accountNumber, 'branch': $routeParams.branch}))
-            .success(function(data) {
-              $scope.clients = data;
-            });
-    $scope.searchType = 'advancedSearch';
-    $scope.parentQuery = $scope.search.advancedQuery;
-  }]);
-
-clientsControllers.controller('ClientDetailsAdvCtrl', ['$scope', '$http', '$routeParams', '$rootScope',
-  function ClientDetailsAdvCtrl($scope, $http, $routeParams, $rootScope) {
-    $http.get(Routing.generate('cr_frontend_client_getclient', {clientId: $routeParams.clientId})).success(function(data) {
-      $scope.client = data;
-    });
-    $scope.urlList = $rootScope.path('advancedClientList', {
-      name: $routeParams.name,
-      birthDate: $routeParams.birthDate,
-      fininstitut: $routeParams.fininstitut,
-      panNumber: $routeParams.panNumber,
-      accountNumber: $routeParams.accountNumber,
-      branch: $routeParams.branch,
-    });
-    $scope.searchType = 'advancedSearch';
-  }]);
+]);
